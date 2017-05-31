189fb4f6b75e1f7f8db59aaf9e4f669abae0cc35
kazmigithub <khairulklang@gmail.com>
2017-06-01 06:45:06 +0800

    Update centos6-kvm-backup1jun17.sh

--- centos6-kvm-backup1jun17.sh
+++ centos6-kvm-backup1jun17.sh
@@ -99,39 +99,34 @@ service php-fpm restart
 service nginx restart
 
 # install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
+apt-get install openvpn -y
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/mappakkoe09/debian/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/arieonline/autoscript/master/conf/1194-centos.conf"
-if [ "$OS" == "x86_64" ]; then
-  wget -O /etc/openvpn/1194.conf "https://raw.github.com/arieonline/autoscript/master/conf/1194-centos64.conf"
-fi
-wget -O /etc/iptables.up.rules "https://raw.github.com/arieonline/autoscript/master/conf/iptables.up.rules"
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.d/rc.local
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '127.0.0.2'`;
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-sed -i $MYIP2 /etc/iptables.up.rules;
-sed -i 's/venet0/eth0/g' /etc/iptables.up.rules
-iptables-restore < /etc/iptables.up.rules
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/mappakkoe09/debian/master/conf/1194.conf"
 service openvpn restart
-chkconfig openvpn on
-cd
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+wget -O /etc/iptables.conf "https://raw.github.com/mappakkoe09/debian/master/conf/iptables.conf"
+sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
+
+myip2="s/ipserver/$myip/g";
+sed -i $myip2 /etc/iptables.conf;
+
+iptables-restore < /etc/iptables.conf
+service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/arieonline/autoscript/master/conf/1194-client.conf"
-sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
-PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-useradd -M -s /bin/false admin
-echo "admin:$PASS" | chpasswd
-echo "admin" > pass.txt
-echo "$PASS" >> pass.txt
-tar cf client.tar 1194-client.ovpn pass.txt
-cp client.tar /home/vps/public_html/
-cd
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/mappakkoe09/debian/master/conf/1194-client.conf"
+usermod -s /bin/false mail
+echo "MuLuu09" | chpasswd
+useradd -s /bin/false -M MuLuu09
+echo "12345" | chpasswd
+#tar cf client.tar 1194-client.ovpn
+cp /etc/openvpn/1194-client.ovpn /home/vps/public_html/
+sed -i $myip2 /home/vps/public_html/1194-client.ovpn
+sed -i "s/ports/55/" /home/vps/public_html/1194-client.ovpn
 
 # install badvpn
 wget -O /usr/bin/badvpn-udpgw "https://raw.github.com/arieonline/autoscript/master/conf/badvpn-udpgw"
